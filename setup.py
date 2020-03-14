8cfeb503929d288140fb330d1b722ded89874a8f
volker <vohegg@gmail.com>
2020-03-14 19:32:28 +0100

    change s.th

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='qelectrotech_labels_to_file',
-    version='1.0',
+    version='',
     packages=[''],
     url='',
-    license='CC BY 3.0 DE',
+    license='',
     author='Volker Heggemann ',
-    author_email='vohegg@gmail.com',
-    description='Reads Qelectrotech Labels out of the Directoryfiles and store them into a qet_labels.xml'
+    author_email='',
+    description=''
 )
